479f0aad35c14d52d87bf2ac112da0e0cf391e55
이종환 <whdghks970731@naver.com>
2018-09-08 14:39:56 +0900

    음성지원

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.user.myapplication;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+import static android.media.audiofx.Visualizer.ERROR;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != ERROR) {
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                tts.setPitch(1.0f);
+                tts.setSpeechRate(1.0f);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ttsGreater21(editText.getText().toString());
+                } else {
+                    ttsUnder20(editText.getText().toString());
+                }
+            }
+        });
+    }
+
+    @SuppressWarnings("deprecation")
+    private void ttsUnder20(String text) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void ttsGreater21(String text) {
+        String utteranceId = this.hashCode() + "";
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+}
